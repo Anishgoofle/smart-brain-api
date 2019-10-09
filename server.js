39aa4f955755ff760f6df560466b6704ad261eba
Anish Ojha <anishojha82@gmail.com>
2019-10-09 17:21:13 +0530

    changes for server

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const apps = new Clarifai.App({
 	apiKey: 'a070ae18896c4d9d95d914fd5bb91386'
 });
 
+const port = process.env.PORT || 3000;
 
 
 const knex = require('knex');
@@ -25,22 +26,27 @@ const db = knex({
 });
 
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => { res.send('it is working!')})
+app.get('/', (req, res) => {
+	res.send('it is working!');
+});
 //imageurl
 app.post('/imageurl', (req, res) => {
 	apps.models.predict(Clarifai.FACE_DETECT_MODEL,
-		req.body.input)
+			req.body.input)
 		.then(data => {
 			res.json(data);
 		})
-		.catch(err => res.status(400).json('unable to work with api'))
-})
+		.catch(err => res.status(400).json('unable to work with api'));
+});
 
 //Sigin
 app.post('/signin', (req, res) => {
-	const { email, password } = req.body;
+	const {
+		email,
+		password
+	} = req.body;
 	if (!email || !password) {
 		return res.status(400).json('incorrect form submission');
 	}
@@ -52,78 +58,88 @@ app.post('/signin', (req, res) => {
 				return db.select('*').from('users')
 					.where('email', '=', email)
 					.then(user => {
-						res.json(user[0])
+						res.json(user[0]);
 					})
-					.catch(err => res.status(400).json('unable to get user'))
+					.catch(err => res.status(400).json('unable to get user'));
 			} else {
-				res.status(400).json('wrong credentials')
+				res.status(400).json('wrong credentials');
 			}
 		})
-		.catch(err => res.status(400).json('wrong credentials'))
-})
+		.catch(err => res.status(400).json('wrong credentials'));
+});
 
 //Register
 app.post('/register', (req, res) => {
-	const { email, name, password } = req.body;
+	const {
+		email,
+		name,
+		password
+	} = req.body;
 	if (!email || !name || !password) {
 		return res.status(400).json('incorrect form submission');
 	}
 	const hash = bcrypt.hashSync(password);
 	db.transaction(trx => {
-		trx.insert({
-			hash: hash,
-			email: email
+			trx.insert({
+					hash: hash,
+					email: email
+				})
+				.into('login')
+				.returning('email')
+				.then(loginEmail => {
+
+					return trx('users')
+						.returning('*')
+
+						.insert({
+							email: loginEmail[0],
+							name: name,
+							joined: new Date()
+						})
+
+						.then(user => {
+							res.json(user[0]);
+						});
+				})
+				.then(trx.commit)
+				.catch(trx.rollback);
 		})
-			.into('login')
-			.returning('email')
-			.then(loginEmail => {
-
-				return trx('users')
-					.returning('*')
-
-					.insert({
-						email: loginEmail[0],
-						name: name,
-						joined: new Date()
-					})
-
-					.then(user => {
-						res.json(user[0]);
-					})
-			})
-			.then(trx.commit)
-			.catch(trx.rollback)
-	})
 		.catch(err => res.status(400).json('unable to register'));
-})
+});
 
 //Profile/id
 app.get('/profile/:id', (req, res) => {
-	const { id } = req.params;
-	db.select('*').from('users').where({ id })
+	const {
+		id
+	} = req.params;
+	db.select('*').from('users').where({
+			id
+		})
 		.then(user => {
 			if (user.length) {
-				res.json(user[0])
+				res.json(user[0]);
 			} else {
-				res.status(400).json("User not found")
+				res.status(400).json("User not found");
 			}
 		})
-		.catch(err => res.status(400).json("error getting user"))
-})
+		.catch(err => res.status(400).json("error getting user"));
+});
 
 //image
 app.put('/image', (req, res) => {
-	const { id } = req.body;
+	const {
+		id
+	} = req.body;
 	db('users').where('id', '=', id)
 		.increment('entries', 1)
 		.returning('entries')
 		.then(entries => {
-			res.json(entries[0])
+			res.json(entries[0]);
 		})
-		.catch(err => res.status(400).json('unable to get entries'))
-})
+		.catch(err => res.status(400).json('unable to get entries'));
+});
 
 
-app.listen(process.env.PORT || 3000, () => {
-	console.log(`app is running on PORT ${process.env.PORT}`);
-})
+app.listen(port, () => {
+	console.log(`app is running on port ${port}!`);
+});
