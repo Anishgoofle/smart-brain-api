12c643848a29d4605909ca4d1dd4058196b11f56
Anish Ojha <anishojha82@gmail.com>
2019-10-11 11:57:16 +0530

    updated database

--- server.js
+++ server.js
@@ -1,145 +1,137 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const clarifai = require('clarifai');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const clarifai = require("clarifai");
 
 const app = express();
 
 const apps = new Clarifai.App({
-	apiKey: 'a070ae18896c4d9d95d914fd5bb91386'
+  apiKey: "a070ae18896c4d9d95d914fd5bb91386"
 });
 
 const port = process.env.PORT || 3000;
 
-
-const knex = require('knex');
+const knex = require("knex");
 
 const db = knex({
-	client: 'pg',
-	connection: {
-		host: '127.0.0.1',
-		user: 'postgres',
-		password: 'admin',
-		database: 'smartbrain_db'
-	}
+  client: "pg",
+  connection: {
+    host: "postgresql-rectangular-88483", //127.0.0.1 for localhost
+    user: "postgres",
+    password: "admin",
+    database: "smartbrain_db"
+  }
 });
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-	res.send('it is working!');
+app.get("/", (req, res) => {
+  res.send("it is working!");
 });
 //imageurl
-app.post('/imageurl', (req, res) => {
-	apps.models.predict(Clarifai.FACE_DETECT_MODEL,
-			req.body.input)
-		.then(data => {
-			res.json(data);
-		})
-		.catch(err => res.status(400).json('unable to work with api'));
+app.post("/imageurl", (req, res) => {
+  apps.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json("unable to work with api"));
 });
 
 //Sigin
-app.post('/signin', (req, res) => {
-	const {
-		email,
-		password
-	} = req.body;
-	if (!email || !password) {
-		return res.status(400).json('incorrect form submission');
-	}
-	db.select('email', 'hash').from('login')
-		.where('email', '=', email)
-		.then(data => {
-			const isValid = bcrypt.compareSync(password, data[0].hash);
-			if (isValid) {
-				return db.select('*').from('users')
-					.where('email', '=', email)
-					.then(user => {
-						res.json(user[0]);
-					})
-					.catch(err => res.status(400).json('unable to get user'));
-			} else {
-				res.status(400).json('wrong credentials');
-			}
-		})
-		.catch(err => res.status(400).json('wrong credentials'));
+app.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("incorrect form submission");
+  }
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    })
+    .catch(err => res.status(400).json("wrong credentials"));
 });
 
 //Register
-app.post('/register', (req, res) => {
-	const {
-		email,
-		name,
-		password
-	} = req.body;
-	if (!email || !name || !password) {
-		return res.status(400).json('incorrect form submission');
-	}
-	const hash = bcrypt.hashSync(password);
-	db.transaction(trx => {
-			trx.insert({
-					hash: hash,
-					email: email
-				})
-				.into('login')
-				.returning('email')
-				.then(loginEmail => {
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json("incorrect form submission");
+  }
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx
+      .insert({
+        hash: hash,
+        email: email
+      })
+      .into("login")
+      .returning("email")
+      .then(loginEmail => {
+        return trx("users")
+          .returning("*")
 
-					return trx('users')
-						.returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
 
-						.insert({
-							email: loginEmail[0],
-							name: name,
-							joined: new Date()
-						})
-
-						.then(user => {
-							res.json(user[0]);
-						});
-				})
-				.then(trx.commit)
-				.catch(trx.rollback);
-		})
-		.catch(err => res.status(400).json('unable to register'));
+          .then(user => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch(err => res.status(400).json("unable to register"));
 });
 
 //Profile/id
-app.get('/profile/:id', (req, res) => {
-	const {
-		id
-	} = req.params;
-	db.select('*').from('users').where({
-			id
-		})
-		.then(user => {
-			if (user.length) {
-				res.json(user[0]);
-			} else {
-				res.status(400).json("User not found");
-			}
-		})
-		.catch(err => res.status(400).json("error getting user"));
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({
+      id
+    })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("User not found");
+      }
+    })
+    .catch(err => res.status(400).json("error getting user"));
 });
 
 //image
-app.put('/image', (req, res) => {
-	const {
-		id
-	} = req.body;
-	db('users').where('id', '=', id)
-		.increment('entries', 1)
-		.returning('entries')
-		.then(entries => {
-			res.json(entries[0]);
-		})
-		.catch(err => res.status(400).json('unable to get entries'));
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("unable to get entries"));
 });
 
-
 app.listen(port, () => {
-	console.log(`app is running on port ${port}!`);
+  console.log(`app is running on port ${port}!`);
 });
